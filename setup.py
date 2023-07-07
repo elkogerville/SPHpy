6a7018ff6d3d4e7bd86da0de9ea8c5283b126fd0
Elko <129769199+elkogerville@users.noreply.github.com>
2023-07-07 17:07:53 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+     name = "SPHpy",
+     version = "0.1",
+     author = "Elko Gerville-Reache",
+     author_email = "elko.gerville-reache@yale.edu",
+     license='MIT',
+     description = "python program that performs smoothed particle hydrodynamical simulations based on Philip Mocz derivations",
+     packages = ["SPHpy"]
+)
